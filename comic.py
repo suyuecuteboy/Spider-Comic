d22880bdd6560424014d052693be6f622996ea03
suyuecuteboy <31234347+suyuecuteboy@users.noreply.github.com>
2017-08-31 20:34:02 +0800

    爬取二次元图片

--- comic.py
+++ comic.py
@@ -2,69 +2,56 @@ import requests
 from urllib.request import urlretrieve
 from urllib.parse import urlparse
 import re
-#导入需要的库和包
 
 
 class Spider():
-#创建一个类
-
-
-
     HEADERS = {
-        'User-Agent': '自定义User-Agent',
+        'User-Agent': '自定义 User-Agent',
         'referer': 'https://www.pangci.cc/works/27192/'
     }
-# 定义类的实例
 
-
-
-    def __init__(self, url):
+    def __init__(self, url='https://www.pangci.cc/works/index_1.html', page=1):
         self.url = url
+        self.page = page
+        self.urls = []
         self.links = set()
         parse_url = urlparse(url)
-        self.base_url = '{}://{}'.format(parse_url[0], parse_url[1])    
-# 初始化工作
-
-
+        self.base_url = '{}://{}'.format(parse_url[0], parse_url[1])
 
     def start(self):
+        self._obtain_urls()
         self._extract_links()
-        self._upload_img() 
-# 启动爬虫
+        self._extract_data()
 
-
+    def _obtain_urls(self):
+        for i in range(1, self.page + 1):
+            self.urls.append(self.url.replace('index_1.html', 'index_{}.html'.format(i)))
 
     def _extract_links(self):
-        rsp = requests.get(self.url, headers=Spider.HEADERS)
-        if rsp.status_code == 200:
-            html = rsp.text
-            links = re.findall(r'<div class="p"><a href="(.*?)" target="_blank"><img src="https', html)
-            for link in links:
-                self.links.add('{}{}'.format(self.base_url, link))
-#获取url
+        for url in self.urls:
+            rsp = requests.get(url, headers=Spider.HEADERS)
+            if rsp.status_code == 200:
+                html = rsp.text
+                links = re.findall(r'<div class="p"><a href="(.*?)" target="_blank"><img src="https', html)
+                for link in links:
+                    self.links.add('{}{}'.format(self.base_url, link))
 
-
-    def _extract_data(self, url):
-        rsp = requests.get(url, headers=Spider.HEADERS)
-        if rsp.status_code == 200:
-            html = rsp.text
-            links = re.findall(
-                r'<li style="margin-top:0px;"><a class="piclink" href=".*?" target="_blank"><img src="(.*?)" alt="(.*?)" ',
-                html)
-            for link in links:
-                img_url = link[0]
-                img_name = link[1]
-                filename = r'胖次网图片/{}.jpg'.format(img_name)
-                urlretrieve(img_url, filename)
-#获取图片并上传
-
-
-    def _upload_img(self):
+    def _extract_data(self):
         for url in self.links:
-            self._extract_data(url)
-#批量上传图片
+            rsp = requests.get(url, headers=Spider.HEADERS)
+            if rsp.status_code == 200:
+                html = rsp.text
+                links = re.findall(
+                    r'<li style="margin-top:0px;"><a class="piclink" href=".*?" target="_blank"><img src="(.*?)" alt="(.*?)" ',
+                    html)
+                for link in links:
+                    img_url = link[0]
+                    img_name = link[1]
+                    print('正在下载{}'.format(img_name))
+                    filename = r'二次元/{}.jpg'.format(img_name)
+                    urlretrieve(img_url, filename)
 
 
 if __name__ == '__main__':
-    spider = Spider('https://www.pangci.cc/user/3749/works.html')
+    spider = Spider()
     spider.start()
